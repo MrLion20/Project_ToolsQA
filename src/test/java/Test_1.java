568bc4ded7aa15aeb6eccff8d299ddfb1bcf6bb7
justin.leon.GB <justin.leon@grupobabel.com>
2021-05-21 11:06:41 -0600

    Listo TEst de Cargar Pagina

--- src/test/java/Test_1.java
+++ src/test/java/Test_1.java
@@ -2,6 +2,7 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
@@ -15,8 +16,16 @@ public class Test_1 {
     public void BeforeSuit() {
         pageobject.BeforeSuit();
     }
-    @Test
+
+    @Test(priority = 1)
     public void test_1() throws InterruptedException {
         pageobject.CargaPagina();
+    }
+    @AfterSuite
+    public void AfterSuite() {
+        pageobject.AfterSuit();
+        driver.manage().deleteAllCookies();
+        driver.close();
+        driver.quit();
     }
 }
