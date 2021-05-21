1b7507c4ecdafe9c455f87397642a592daf35175
justin.leon.GB <justin.leon@grupobabel.com>
2021-05-21 10:39:04 -0600

    actualizar Pom

--- src/test/java/Test_1.java
+++ src/test/java/Test_1.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,15 +8,12 @@ public class Test_1 {
     private static final WebDriver driver = new ChromeDriver();
     PageObject pageobject = new PageObject(driver);
 
-    @BeforeAll
-    public void setup(){
+
+    @Test
+    public void test_1() throws InterruptedException {
         pageobject.visit("https://demoqa.com/");
         // aca se maximisa la ventana
         driver.manage().window().maximize();
-
-    }
-    @Test
-    public void test_1() throws InterruptedException {
         pageobject.CargaPagina();
     }
 }
