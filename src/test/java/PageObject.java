2c7b8882d8eef2eba348347f7ddfa36f28de93a2
justin.leon.GB <justin.leon@grupobabel.com>
2021-05-21 10:59:56 -0600

    Actualizar y Crear el Test de cargar pagina y lo de Before Suit

--- src/test/java/PageObject.java
+++ src/test/java/PageObject.java
@@ -1,13 +1,19 @@
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 
 public class PageObject extends Basepage {
 
     public PageObject(WebDriver driver) {
         super(driver);
     }
+
+    ExtentReports extent;
+    ExtentSparkReporter sparkReporter;
+
     //-----------------------Indicadores necesarios para las pruebas  ---------------------------------
     //indicadores para ingresar a la pagina de Formulario para realizar las pruebas correspondientes
 
@@ -16,15 +22,36 @@ public class PageObject extends Basepage {
     By VentanaTitle = By.xpath("//*[@id=\"app\"]/div/div/div[2]/div[2]/div[1]/h5");
 
     //-------------------implementacion de funciones para las pruebas --------------------------
-    //--------------Esperamos que la pagina Cargue por completo nos dirigimos a el boton que debemos e seleccionar
 
+
+    //Acciones Antes Pruebas
+    public void BeforeSuit() {
+        extent = new ExtentReports();
+        sparkReporter = new ExtentSparkReporter("Reports\\Report_ToolsQA.html");
+        extent.attachReporter(sparkReporter);
+        sparkReporter.config().setDocumentTitle("Prueba de Pagina ToolsQA");
+
+        extent.setSystemInfo("Company name", "Babel");
+        extent.setSystemInfo("Proyect Name", "ToolsQA Page");
+        extent.setSystemInfo("Tester Name", "Ing. Justin Leon Castro");
+        extent.setSystemInfo("Test Lead", "Ing. ..............");
+    }
+
+    //Acciones Despues Pruebas
+    public void AfterSuit() {
+        extent.flush();
+    }
+
+    //Test Cargar Pagina
     public void CargaPagina() throws InterruptedException {
+        ExtentTest test = extent.createTest("Iniciar Prueba Cargar_Pagina");
+        System.setProperty("webdriver.chrome.driver", Conexion.CHROME_DRIVER_LOCATION);
+        test.log(Status.INFO, "Abierto navegador");
         Thread.sleep(2000);
-        //Esta funcion lo que realiza es buscar el elemento que deseamos y dirigirnos hacia el para luego tomar alguna accion
-        WebElement element = driver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div[2]/div/div[2]"));
-        Actions actions = new Actions(driver);
-        actions.moveToElement(element);
-        actions.perform();
-        Thread.sleep(2000);
+        driver.get(Conexion.BASE_URL);
+        test.info("Se Abre la URL de prueba" + "https://demoqa.com/");
+        driver.manage().window().maximize();
+        test.info("Se maximiza la ventana del navegador");
+        test.pass("Prueba Cargar_Pagina Completa");
     }
 }
--- src/test/java/Test_1.java
+++ src/test/java/Test_1.java
@@ -1,19 +1,22 @@
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
-;
-
 public class Test_1 {
+    ExtentReports extent;
+    ExtentSparkReporter sparkReporter;
     private static final WebDriver driver = new ChromeDriver();
     PageObject pageobject = new PageObject(driver);
 
-
+    @BeforeSuite
+    public void BeforeSuit() {
+        pageobject.BeforeSuit();
+    }
     @Test
     public void test_1() throws InterruptedException {
-        pageobject.visit("https://demoqa.com/");
-        // aca se maximisa la ventana
-        driver.manage().window().maximize();
         pageobject.CargaPagina();
     }
 }
