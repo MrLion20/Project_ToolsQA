347e8dfd6d5cfd67490ef2241c16efcfef40ad05
justin.leon.GB <justin.leon@grupobabel.com>
2021-05-21 10:22:42 -0600

    actualizar Pom

--- src/test/java/Test_1.java
+++ src/test/java/Test_1.java
@@ -1,16 +1,14 @@
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test_1 {
-    private WebDriver driver;
-    PageObject pageobject;
-
+    private static final WebDriver driver = new ChromeDriver();
+    PageObject pageobject = new PageObject(driver);
 
     @Before
     public void setup(){
-        PageObject pageobject = new PageObject(driver);
-        driver = pageobject.chromeDriverConnection();
         pageobject.visit("https://demoqa.com/");
         // aca se maximisa la ventana
         driver.manage().window().maximize();
